b13c3ccbc0abad49880737a1f633d6abec8a1bfc
joestack <joern@joern-online.de>
2023-02-01 13:01:12 +0100

    first commit 3

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,11 @@ output "vault_token" {
   value     = hcp_vault_cluster_admin_token.example.token
   sensitive = true
 }
+
+output "vault_address" {
+  value = hcp_vault_cluster.example.public_endpoint
+}
+
+output "vault_namespace" {
+  value = hcp_vault_cluster.example.namespace
+}
